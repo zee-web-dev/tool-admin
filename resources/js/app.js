ff06c76d98aed5119e41434feac01c4bfce7c5c7
ajwa <ajwa082@gmail.com>
2023-07-28 20:15:00 +0500

    change template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import persist from '@alpinejs/persist'
-
-Alpine.plugin(persist)
 
 window.Alpine = Alpine;
 
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.content.dashboard.index');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.content.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
